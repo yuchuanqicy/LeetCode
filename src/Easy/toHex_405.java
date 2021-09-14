5c7d16be8105d366089151a45a415101edf18b93
ycq <1334729724@qq.com>
2021-09-14 23:41:35 +0800

    新增

--- src/Easy/toHex_405.java
+++ src/Easy/toHex_405.java
@@ -9,7 +9,8 @@ public class toHex_405 {
     public static void main(String[] args) {
         String res=toHex(-1);
         System.out.println(res);
-
+        String res2=toHex2(-1);
+        System.out.println(res2);
     }
     public  static  String toHex(int nums){
         if (nums==0)
@@ -25,11 +26,27 @@ public class toHex_405 {
         return sb.toString();
     }
     public static String toHex2(int num){
+        int temp;
+        StringBuffer sb = new StringBuffer();
         if (num==0)
             return "0";
-        String s=new String();
-
-        return s;
+        while (num!=0){
+            temp= num & 15;
+        }
+        return sb.toString();
+    }
+    public String toHex3(int num) {
+        StringBuilder bl = new StringBuilder();
+        if (num == 0) {
+            bl.insert(0, 0);
+        }
+        long y = num < 0 ? num & 0x00000000ffffffffL : num;
+        while (y != 0) {
+            char hex = (char) (y % 16 < 10 ? y % 16 + '0' : y % 16 - 10 + 'a');
+            bl.insert(0, hex);
+            y >>= 4;
+        }
+        return bl.toString();
     }
 
 }

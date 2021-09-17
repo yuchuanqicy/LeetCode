109453a394279ccfb971789275c0630e3b61697d
yuchuanqicy <1334729724@qq.com>
2021-09-17 14:48:25 +0800

    Test

---
+++ src/Medium/numberOfArithmeticSlices_413.java
@@ -0,0 +1,13 @@
+package Medium;
+
+public class numberOfArithmeticSlices_413 {
+    public static void main(String[] args) {
+        int list [] ={1,2,3,4,5};
+        numberOfArithmeticSlices(list);
+    }
+
+    public static int numberOfArithmeticSlices(int list []){
+
+        return 1;   
+    }
+}

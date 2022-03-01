ee45186d164678bddf5e380babffe7a5b9aaaa4f
yuchuanqicy <1334729724@qq.com>
2022-03-01 11:29:44 +0800

    488

---
+++ src/Medium/knightProbability_688.java
@@ -0,0 +1,11 @@
+package Medium;
+
+public class knightProbability_688 {
+    public static void main(String[] args) {
+
+    }
+    public  static double knightProbability(int n, int k, int row, int column) {
+
+        return 1.0;
+    }
+}

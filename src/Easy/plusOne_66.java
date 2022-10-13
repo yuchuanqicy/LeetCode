f0c14965dfbb892ea4d0c1fe36a77f21c6953f66
yuchuanqicy <1334729724@qq.com>
2022-10-13 20:26:18 +0800

    66

---
+++ src/Easy/plusOne_66.java
@@ -0,0 +1,14 @@
+package Easy;
+
+/**
+ * @ClassNmae plusOne_66
+ * @Author:yys4608
+ * @Date: 2022/10/13 20:18
+ * @Description:整数+1
+ */
+public class plusOne_66 {
+    public int[] plusOne(int[] digits) {
+
+        return null;
+    }
+}
--- src/main/Test.java
+++ src/main/Test.java
@@ -1,12 +1,11 @@
 package main;
-import Algorithm_implementation.Bubble_Sort;
-import Algorithm_implementation.SelectSort;
-import Easy.RecentCounter_933;
-import Easy.binaryGap_868;
-import Medium.Solution_398;
-import Medium.lengthLongestPath_388;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+
+import Easy.lengthOfLastWord_58;
 import  Easy.toGoatLatin_824;
+import Medium.lengthLongestPath_388;
 
 /**
  * @Author: yuchuanqi
@@ -15,9 +14,8 @@ import  Easy.toGoatLatin_824;
  */
 public class Test {
     public static void main(String[] args) {
-        Bubble_Sort bubble_sort=new Bubble_Sort();
-        SelectSort selectSort=new SelectSort();
-        int [] test=new int[]{20,32,6,1,8,9,10};
-        System.out.println(Arrays.toString(selectSort.Select_Sort(test)));
+        lengthOfLastWord_58 lengthOfLastWord_58 = new lengthOfLastWord_58();
+        System.out.println(lengthOfLastWord_58.lengthOfLastWord("   fly me   to   the moon  "));
+
     }
 }

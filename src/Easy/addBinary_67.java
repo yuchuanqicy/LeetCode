2464f13f0ef8db8204cb6bce77c404fb44b50692
yuchuanqicy <1334729724@qq.com>
2022-10-14 18:00:57 +0800

    66

---
+++ src/Easy/addBinary_67.java
@@ -0,0 +1,22 @@
+package Easy;
+
+/**
+ * @ClassNmae addBinary_67
+ * @Author:yys4608
+ * @Date: 2022/10/14 10:12
+ * @Description:二进制加法
+ */
+public class addBinary_67 {
+    public String addBinary(String a, String b) {
+        int len1=a.length();
+        int len2=b.length();
+        int n=Math.max(len1,len2);
+        //判断是否进位
+        int ans=0;
+        for (int i =0;i>=0 ;i++) {
+
+        }
+        return null;
+
+    }
+}
---
+++ src/Easy/mySqrt_69.java
@@ -0,0 +1,37 @@
+package Easy;
+
+/**
+ * @ClassNmae mySqrt_69
+ * @Author:yys4608
+ * @Date: 2022/10/14 16:38
+ * @Description:实现X的算术平方根
+ */
+public class mySqrt_69 {
+    public int mySqrt(int x) {
+        long ans=0;
+        for (int i = 0; i*i <= x; i++) {
+            ans=i;
+        }
+        return (int) ans;
+    }
+
+  /*@Author:chuanqiyu
+    *@DateTime 17:06 2022/10/14
+    *@Params
+    *@Retuen
+    */
+    public int mySqrt1(int x){
+        long i=0;
+        int left=0;
+        int right=x-1;
+        int mid=0;
+        while (left<=right){
+            mid=(left+right)/2;
+            if ((long)mid*mid<x){
+
+            }
+        }
+
+        return 0;
+    }
+}
